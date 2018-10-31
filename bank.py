bcebc242d6e9e79854016f123a354850cbc6c76f
natesevn <nikoniko562@gmail.com>
2018-10-31 13:44:16 -0700

    fixed some small typos

--- bank.py
+++ bank.py
@@ -6,7 +6,7 @@ import hashlib
 
 
 print("Hello, welcome to our banking system,\nFor receiving funds from the bank, press '1'\nFor receiving money from another customer, press '2'\n" +
-		"For sending money to another customer, press '3'\nFor synchronizing customers, press '4'\n To quit, type in 'exit'");
+		"For sending money to another customer, press '3'\nFor synchronizing customers, press '4'\nTo quit, type in 'exit'");
 var  = input("Please enter your selection: ")
 while (var != 'exit'):
 
@@ -14,9 +14,9 @@ while (var != 'exit'):
 	if var == 'exit':
 		exit()
 	if var == '1':
-		print("You chose to receiv funds from the bank:")
+		print("You chose to receive funds from the bank:")
 
-		ss = input("Enter your toekn here: ");
+		ss = input("Enter your token here: ");
 		cipher = AESCipher()
 		token = bytes.fromhex(ss)
 
@@ -39,7 +39,7 @@ while (var != 'exit'):
 		
 	elif var == '2':
 		print("You chose to receive money from another customer:");
-		token = input("Enter your toekn here: ");
+		token = input("Enter your token here: ");
 		#token = hexlify(b'a41256731a3ed2b725629023e1201055')
 		#myId = input("Enter your id: ");
 		myReceiver = Receiver(token, int(1234) );
@@ -48,22 +48,22 @@ while (var != 'exit'):
 
 	elif var == '3':
 		print("You chose to send money to another customer:");
-		sendID = input("Please enter your Wallet ID: ");
+		sendID = input("Please enter your wallet ID: ");
 		receiverID = input("Please enter your receiver ID: ");
 		amount = input("Finally, please enter the amount you would like to send to receiver "+receiverID+ ": ");
 		mySender =  Sender(sendID, receiverID, amount)
 		token = mySender.sendMoney();
 		print("Your token is " + str(token) + ", present it to your receiver.");
 	else :
-		print("You chose to synchornzie the users:");
+		print("You chose to synchronize the users:");
 
-		sendID = input("Please enter your Wallet ID: ");
+		sendID = input("Please enter your wallet ID: ");
 		receiverID = input("Please enter your receiver ID: ");
 		mySender =  Sender(sendID, receiverID, int(0))
 		token = mySender.sendMoney();
 		print("Your token is " + str(token));
 		
-		token = input("Now Enter the token you received from the other user: ");
+		token = input("Now enter the token you received from the other user: ");
 		#token = hexlify(b'a41256731a3ed2b725629023e1201055')
 		myId = input("Enter your id: ");
 		myReceiver = Receiver(token, int(myId));
